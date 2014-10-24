938da06638ec4d043f18155b1fb205e6c86b6331
Andrew Ripka <aripka@pivotal.io>
2014-10-23 23:13:50 -0500

    enable security

---
+++ src/main/java/com/rippmn/halloween/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.rippmn.halloween;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/track.html").hasAnyRole("PC_REZ");
+        http.formLogin();
+        
+    }
+
+    @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("rippmn").password("passw0rd!").roles("PC_REZ").and()
+                    .withUser("user2").password("passw0rd!").roles("PC_REZ");
+        }
+
+    }
+
+}
