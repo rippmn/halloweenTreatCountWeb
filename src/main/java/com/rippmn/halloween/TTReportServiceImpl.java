c011b34fe749de7b67c33341c9832299c11550ad
Andrew Ripka <aripka@pivotal.io>
2014-10-28 00:40:56 -0500

    update to have logic (ready for testing)

--- src/main/java/com/rippmn/halloween/TTReportServiceImpl.java
+++ src/main/java/com/rippmn/halloween/TTReportServiceImpl.java
@@ -3,25 +3,21 @@ package com.rippmn.halloween;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 import org.springframework.web.client.RestTemplate;
 
 public class TTReportServiceImpl implements TrickOrTreaterReportService {
 
-	//TrickOrTreatReportingEvent latest;
-	ArrayList<TrickOrTreatReportingEvent> ttres = new ArrayList<TrickOrTreatReportingEvent>();
+	ArrayList<TrickOrTreatReportingEvent> ttres; //= new ArrayList<TrickOrTreatReportingEvent>();
 	TreeMap<Integer, TrickOrTreatReportingEvent> ttresByTime = new TreeMap<Integer, TrickOrTreatReportingEvent>();
 	
 	@Override
 	public List<TrickOrTreatReportingEvent> getAllEvents() {
-		//RestTemplate restTemplate = new RestTemplate();
-		//TTEvent[] es = restTemplate.getForObject("http://localhost:8080/getTTs", TTEvent[].class);
-		
-		return null;
-		
+
+		return ttres;
 				
 	}
 	
@@ -38,33 +34,85 @@ public class TTReportServiceImpl implements TrickOrTreaterReportService {
 		
 	}
 	
+	//we will start at 5:00 (there will be event for all years)
+	//every time quartz job fires get the prior year events and transfer to the next cycle
+	
+	
 	
 	public void initialize(){
 		RestTemplate restTemplate = new RestTemplate();
 		TTEvent[] es = restTemplate.getForObject("http://localhost:8080/getTTs", TTEvent[].class);
 		
+		TreeMap<Date, Integer> ttes = new TreeMap<Date, Integer>();
+		
 		for(TTEvent tte: es){
-			TrickOrTreatReportingEvent ttre =  ttresByTime.get(TrickOrTreatReportingEvent.getTime(tte.getEventDateTime()));
-			if(ttre == null){
-				ttre = new TrickOrTreatReportingEvent(tte.getEventDateTime(), tte.getCount());
-				
-				//transfer reporting totals
-				//we cannot do this until we have a set of data  
-				
-				ttresByTime.put(ttre.getTime(), ttre);
-			}else{
-				ttre.putEvent(tte.getEventDateTime(), tte.getCount());
-			}
-			
-			//at the end of this we will have a consoliated count of events by time per haps we whould also make a total available in the same series....
-			
+			ttes.put(tte.getEventDateTime(), tte.getCount());
 		}
 		
-		ttres.addAll(ttresByTime.values());		
-				
+		for(Date d: ttes.keySet()){
+			
+			//everything below here is method
+			this.addEvent(d, ttes.get(d));
+			
+		}
+
+		this.updateList();
+
 	}
 	
-	private void addEvent(TTEvent tte){
-	}
+	private void addEvent(Date d, Integer count){
+		Integer last;
+		TrickOrTreatReportingEvent ttre =  ttresByTime.get(TrickOrTreatReportingEvent.getTime(d));
+		if(ttre == null){
+			ttre = new TrickOrTreatReportingEvent(d, count);
+			ttresByTime.put(ttre.getTime(), ttre);
+			last = ttresByTime.lowerKey(ttre.getTime()) ;
+			if(last != null){
+				//if there is a last event we need to transfer counts
+				Map<String, Integer> priorCounts = ttresByTime.get(last).getYearCounts();
+				
+				for(String year: priorCounts.keySet()){
+					Integer currentCount = ttre.getYearCounts().get(year);
+					
+					if(currentCount == null){
+						ttre.getYearCounts().put(year, priorCounts.get(year));
+					}else{
+						ttre.getYearCounts().put(year, currentCount + priorCounts.get(year));   
+					}
+					
+				}
+				
+			}else{
+				//prior totals are 0 do nothing
+			}
+		}else{
+			ttre.putEvent(d, count);
+		}
 
+	}
+	
+	
+	public void updateReport(){
+		//get the current events from service 
+		TTEvent[] ttes = new TTEvent[1];
+		
+		for (TTEvent tte: ttes) {
+			this.addEvent(tte.getEventDateTime(), tte.getCount());
+		}
+	
+		this.updateList();
+		
+		
+	}
+	
+	private void updateList(){
+		if(ttresByTime.floorKey(TrickOrTreatReportingEvent.getTime(new Date())) != null){
+			//the returnable subset to be up to date
+			ttres = new ArrayList<TrickOrTreatReportingEvent>(ttresByTime.headMap(TrickOrTreatReportingEvent.getTime(new Date()), false).values());				
+		}else{
+			ttres = new ArrayList<TrickOrTreatReportingEvent>();
+			ttres.add(ttresByTime.firstEntry().getValue());
+		}
+	}
+	
 }
