6417e0b283b72e22fdbfd9e99bca1d99577a25f1
Andrew Ripka <aripka@pivotal.io>
2014-10-29 18:34:37 -0500

    update to call actual batch job

--- src/main/java/com/rippmn/halloween/UpdateReportDataJob.java
+++ src/main/java/com/rippmn/halloween/UpdateReportDataJob.java
@@ -1,16 +1,18 @@
 package com.rippmn.halloween;
 
-import java.util.Date;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 public class UpdateReportDataJob{
 
+		@Autowired
+		TrickOrTreaterReportService service;
+	
 		@Scheduled(cron="0 0/2 * * * *")
 		public void theTask(){
-			System.out.println("*****"+new Date());
+			service.updateReport();
 		}
 
 }
