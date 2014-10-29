7f2d5341cf2a9a428b479ae8baa2f71950e47d94
Andrew Ripka <aripka@pivotal.io>
2014-10-29 12:01:07 -0500

    update update for new year data add

--- src/main/java/com/rippmn/halloween/TTReportServiceImpl.java
+++ src/main/java/com/rippmn/halloween/TTReportServiceImpl.java
@@ -14,108 +14,138 @@ public class TTReportServiceImpl implements TrickOrTreaterReportService {
 
 	@Autowired
 	private TrickOrTreatEventService ttes;
-	
-	private ArrayList<TrickOrTreatReportingEvent> ttres; //= new ArrayList<TrickOrTreatReportingEvent>();
+
+	public void setTrickORTreatEventService(TrickOrTreatEventService ttes) {
+		this.ttes = ttes;
+	}
+
+	private ArrayList<TrickOrTreatReportingEvent> ttres; // = new
+															// ArrayList<TrickOrTreatReportingEvent>();
 	private TreeMap<Integer, TrickOrTreatReportingEvent> ttresByTime = new TreeMap<Integer, TrickOrTreatReportingEvent>();
-	
+
 	@Override
 	public List<TrickOrTreatReportingEvent> getAllEvents() {
 
 		return ttres;
-				
+
 	}
-	
-	public static void main(String[] args)throws Exception{
-		TTReportServiceImpl tt = new TTReportServiceImpl();
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-		
-		Date d = sdf.parse("2014-10-31 18:00:01");
-		
-		tt.initialize();
-		
-		System.out.println("done");
-		
-	}
-	
-	//we will start at 5:00 (there will be event for all years)
-	//every time quartz job fires get the prior year events and transfer to the next cycle
-	
-	public void initialize(){
+
+	// we will start at 5:00 (there will be event for all years)
+	// every time quartz job fires get the prior year events and transfer to the
+	// next cycle
+
+	public void initialize() {
 		TTEvent[] es = ttes.getAllTTEvents();
-		
+
 		TreeMap<Date, Integer> ttes = new TreeMap<Date, Integer>();
-		
-		for(TTEvent tte: es){
+
+		for (TTEvent tte : es) {
 			ttes.put(tte.getEventDateTime(), tte.getCount());
 		}
-		
-		for(Date d: ttes.keySet()){
-			
-			//everything below here is method
+
+		for (Date d : ttes.keySet()) {
+
+			// everything below here is method
 			this.addEvent(d, ttes.get(d));
-			
+
+		}
+
+		// now we need to update the totals (transfer prior counts)
+		TrickOrTreatReportingEvent lastEvent = null;
+		for (TrickOrTreatReportingEvent event : ttresByTime.values()) {
+			if (lastEvent != null) {
+				for (String year : lastEvent.getYearCounts().keySet()) {
+					int lastCount = lastEvent.getYearCounts().get(year);
+					int currentCount = event.getYearCounts().get(year) != null ? event
+							.getYearCounts().get(year) : 0;
+					event.getYearCounts().put(year, lastCount + currentCount);
+				}
+			}
+			lastEvent = event;
+
 		}
 
 		this.updateList();
 
 	}
-	
-	private void addEvent(Date d, Integer count){
+
+	private void addEvent(Date d, Integer count) {
 		Integer last;
-		TrickOrTreatReportingEvent ttre =  ttresByTime.get(TrickOrTreatReportingEvent.getTime(d));
-		if(ttre == null){
+		TrickOrTreatReportingEvent ttre = ttresByTime
+				.get(TrickOrTreatReportingEvent.getTime(d));
+		if (ttre == null) {
 			ttre = new TrickOrTreatReportingEvent(d, count);
 			ttresByTime.put(ttre.getTime(), ttre);
-			last = ttresByTime.lowerKey(ttre.getTime()) ;
-			if(last != null){
-				//if there is a last event we need to transfer counts
-				Map<String, Integer> priorCounts = ttresByTime.get(last).getYearCounts();
-				
-				for(String year: priorCounts.keySet()){
-					Integer currentCount = ttre.getYearCounts().get(year);
-					
-					if(currentCount == null){
-						ttre.getYearCounts().put(year, priorCounts.get(year));
-					}else{
-						ttre.getYearCounts().put(year, currentCount + priorCounts.get(year));   
-					}
-					
-				}
-				
-			}else{
-				//prior totals are 0 do nothing
-			}
-		}else{
+		} else {
 			ttre.putEvent(d, count);
 		}
 
 	}
-	
-	
-	public void updateReport(){
-		//get the current events from service 
-		//anything in last two mins
-		TTEvent[] ttesEvents = ttes.getEventsAfterDate(new Date(System.currentTimeMillis()-120000));
-		
-		for (TTEvent tte: ttesEvents) {
+
+	public void updateReport() {
+		// get the current events from service
+		// anything in last two mins
+		Date d = new Date(System.currentTimeMillis() - 120000);
+
+		TTEvent[] ttesEvents = ttes.getEventsAfterDate(d);
+
+		for (TTEvent tte : ttesEvents) {
 			this.addEvent(tte.getEventDateTime(), tte.getCount());
 		}
-	
-		this.updateList();
-		
-		
-	}
-	
-	private void updateList(){
-		//TODO this would be cooler if and only if we could add to existing list instead of creating new one (future)
-		if(ttresByTime.floorKey(TrickOrTreatReportingEvent.getTime(new Date())) != null){
-			//the returnable subset to be up to date
-			ttres = new ArrayList<TrickOrTreatReportingEvent>(ttresByTime.headMap(TrickOrTreatReportingEvent.getTime(new Date()), false).values());				
-		}else{
-			ttres = new ArrayList<TrickOrTreatReportingEvent>();
-			ttres.add(ttresByTime.firstEntry().getValue());
+
+		Integer time = TrickOrTreatReportingEvent.getTime(new Date());
+
+		TrickOrTreatReportingEvent event = ttresByTime.get(time);
+		TrickOrTreatReportingEvent lastEvent = ttresByTime.lowerEntry(time)
+				.getValue();
+
+		for (String year : lastEvent.getYearCounts().keySet()) {
+			int lastCount = lastEvent.getYearCounts().get(year);
+			int currentCount = event.getYearCounts().get(year) != null ? event
+					.getYearCounts().get(year) : 0;
+			event.getYearCounts().put(year, lastCount + currentCount);
 		}
+
+		this.updateList();
+
 	}
-	
+
+	private void updateList() {
+		// TODO this would be cooler if and only if we could add to existing
+		// list instead of creating new one (future)
+
+		if (ttres != null) {
+			
+			//need to add the next entry
+			if(ttres.size() > 0){
+				Integer lastKey = ttres.get(ttres.size()-1).getTime();
+				ttres.add(ttresByTime.higherEntry(lastKey).getValue());
+			}
+		
+		} else {// this is the initial load
+			ttres = new ArrayList<TrickOrTreatReportingEvent>();
+
+			Integer currentTime = TrickOrTreatReportingEvent
+					.getTime(new Date());
+			if (ttresByTime.floorKey(currentTime) != null) {
+				// the returnable subset to be up to date
+				ttres = new ArrayList<TrickOrTreatReportingEvent>(ttresByTime
+						.headMap(currentTime).values());
+			}
+		}
+
+		//
+
+		// if (ttresByTime
+		// .floorKey(TrickOrTreatReportingEvent.getTime(new Date())) != null) {
+		// // the returnable subset to be up to date
+		// ttres = new ArrayList<TrickOrTreatReportingEvent>(ttresByTime
+		// .headMap(TrickOrTreatReportingEvent.getTime(new Date()),
+		// false).values());
+		// } else {
+		// ttres = new ArrayList<TrickOrTreatReportingEvent>();
+		// ttres.add(ttresByTime.firstEntry().getValue());
+		// }
+	}
+
 }
--- src/test/java/com/rippmn/halloween/TTReportServiceImplTest.java
+++ src/test/java/com/rippmn/halloween/TTReportServiceImplTest.java
@@ -23,20 +23,22 @@ public class TTReportServiceImplTest {
 	private static final long YEAR = 365l*24*60*60000;
 	
 	private Date[] dates;
+	private long currentTime;
 	
 	private TTEvent[] getTestData() throws Exception {
 		
 		
 		//this needs to be a dynamic create of these so they are relevant to present time.... (note below approach does not deal with past MIDNIGHT as reporting model assumes a calendar day)
 		//setbaseDate to be current time - 3 years - 4 hours
-		long baseTime = System.currentTimeMillis()-(3*YEAR)-(4*HOUR);
+		currentTime=System.currentTimeMillis();
+		long baseTime = currentTime-(3*YEAR)-(4*HOUR);
 		
 		dates = new Date[]{new Date(baseTime+(2*YEAR)), new Date(baseTime+YEAR), new Date(baseTime),
 				new Date(baseTime+(2*YEAR)+HOUR), new Date(baseTime+YEAR+HOUR),
 				new Date(baseTime+YEAR+(2*HOUR)), new Date(baseTime+(2*HOUR)),
-				new Date(baseTime+(2*YEAR)+(3*HOUR)), new Date(baseTime+(3*HOUR))};
+				new Date(baseTime+(2*YEAR)+(3*HOUR)), new Date(baseTime+(3*HOUR)), new Date(currentTime-(4*HOUR))};
 		
-		int[] counts = new int[] { 0, 0, 0, 1, 2, 3, 4, 5, 6 };
+		int[] counts = new int[] { 0, 0, 0, 1, 2, 3, 4, 5, 6,0 };
 		ArrayList<TTEvent> ttes = new ArrayList<TTEvent>(10);
 		TTEvent tte;
 		for (int i = 0; i < dates.length; i++) {
@@ -87,7 +89,7 @@ public class TTReportServiceImplTest {
 		assertEquals(0,
 				service.getAllEvents().get(0).getYearCounts().get("2011")
 						.intValue());
-
+		
 		assertEquals(1,
 				service.getAllEvents().get(1).getYearCounts().get("2013")
 						.intValue());
@@ -118,33 +120,73 @@ public class TTReportServiceImplTest {
 				service.getAllEvents().get(3).getYearCounts().get("2011")
 						.intValue());
 
+		//add 2014 base date
+		assertEquals(0,
+				service.getAllEvents().get(0).getYearCounts().get("2014")
+						.intValue());
+		assertEquals(0,
+				service.getAllEvents().get(1).getYearCounts().get("2014")
+						.intValue());
+		assertEquals(0,
+				service.getAllEvents().get(2).getYearCounts().get("2014")
+						.intValue());
+		assertEquals(0,
+				service.getAllEvents().get(3).getYearCounts().get("2014")
+						.intValue());
+
+	
 	}
 	
-//	@Test
-//	public void testUpdateReportAddToEnd()throws Exception{
-//		MockitoAnnotations.initMocks(this);
-//
-//		TTEvent[] ttes = new TTEvent[1];
-//		ttes[0] = new TTEvent();
-//		ttes[0].setCount(100);
-//		ttes[0].setEventDateTime(new Date());
-//
-//		when(tteService.getAllTTEvents()).thenReturn(getTestData());
-//
-//		TTReportServiceImpl service = new TTReportServiceImpl();
-//		service.setTrickORTreatEventService(tteService);
-//		service.initialize();
-//		when(tteService.getEventsAfterDate(any(Date.class))).thenReturn(ttes);
-//
-//		service.updateReport();
-//		verify(tteService, times(1)).getEventsAfterDate(any(Date.class));
-//		assertEquals(5, service.getAllEvents().size());
-//		assertEquals(TrickOrTreatReportingEvent.getTime(ttes[0].getEventDateTime()), service.getAllEvents().get(4).getTime());
-//		assertEquals(4, service.getAllEvents().get(4).getYearCounts().size());
-//		
-//		
-//		fail("end");
-//
-//	}
+	//note this test may be moot given we should have a base data point for 2014 as well. but we should see 
+	@Test
+	public void testUpdateReportAddToEnd()throws Exception{
+		MockitoAnnotations.initMocks(this);
+		
+		TTEvent[] baseData = getTestData();
+		TTEvent[] ttes = new TTEvent[1];
+		ttes[0] = new TTEvent();
+		ttes[0].setCount(100);
+		ttes[0].setEventDateTime(new Date(currentTime));
+
+		when(tteService.getAllTTEvents()).thenReturn(baseData);
+
+		TTReportServiceImpl service = new TTReportServiceImpl();
+		service.setTrickORTreatEventService(tteService);
+		service.initialize();
+		when(tteService.getEventsAfterDate(any(Date.class))).thenReturn(ttes);
+
+		service.updateReport();
+		verify(tteService, times(1)).getEventsAfterDate(any(Date.class));
+		assertEquals(5, service.getAllEvents().size());
+		assertEquals(TrickOrTreatReportingEvent.getTime(ttes[0].getEventDateTime()), service.getAllEvents().get(4).getTime());
+		assertEquals(4, service.getAllEvents().get(4).getYearCounts().size());
+		
+		assertEquals(6,
+				service.getAllEvents().get(3).getYearCounts().get("2013")
+						.intValue());
+		assertEquals(0,
+				service.getAllEvents().get(3).getYearCounts().get("2014")
+						.intValue());
+		assertEquals(5,
+				service.getAllEvents().get(3).getYearCounts().get("2012")
+						.intValue());
+		assertEquals(10,
+				service.getAllEvents().get(3).getYearCounts().get("2011")
+						.intValue());
+		
+		assertEquals(6,
+				service.getAllEvents().get(4).getYearCounts().get("2013")
+						.intValue());
+		assertEquals(5,
+				service.getAllEvents().get(4).getYearCounts().get("2012")
+						.intValue());
+		assertEquals(10,
+				service.getAllEvents().get(4).getYearCounts().get("2011")
+						.intValue());
+		assertEquals(100,
+				service.getAllEvents().get(4).getYearCounts().get("2014")
+						.intValue());
+
+	}
 
 }
