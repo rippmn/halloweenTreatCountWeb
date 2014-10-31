c9a754def264b656353ba9fa453e6eb27131fc58
Andrew Ripka <aripka@pivotal.io>
2014-10-31 15:53:12 -0500

    add a null checking

--- src/main/java/com/rippmn/halloween/web/TrickOrTreatController.java
+++ src/main/java/com/rippmn/halloween/web/TrickOrTreatController.java
@@ -1,7 +1,5 @@
 package com.rippmn.halloween.web;
 
-import java.util.HashMap;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.rippmn.halloween.domain.TrickOrTreatReportingEvent;
 import com.rippmn.halloween.service.TrickOrTreatEventService;
 import com.rippmn.halloween.service.TrickOrTreaterReportService;
 
@@ -25,24 +24,28 @@ public class TrickOrTreatController {
 	
 	Logger logger = LoggerFactory.getLogger(TrickOrTreatController.class);
 	
-//	@RequestMapping("/trickOrTreat")
-//	public String trickOrTreatLanding(){
-//		return "trickOrTreat";
-//	}
-//	
-//	@RequestMapping(value="/count", method=RequestMethod.POST)
-//	public String count(@RequestParam int count){
-//		System.out.println("count:"+count);
-//		service.trickOrTreatEvent(count);
-//		return "trickOrTreat";
-//	}
+	@RequestMapping("/trickOrTreat")
+	public String trickOrTreatLanding(){
+		return "trickOrTreat";
+	}
+	
+	@RequestMapping(value="/count", method=RequestMethod.POST)
+	public String count(@RequestParam int count){
+		System.out.println("count:"+count);
+		service.trickOrTreatEvent(count);
+		return "trickOrTreat";
+	}
 	
 	@RequestMapping("/report")
 	public ModelAndView report(){
 		
 		ModelAndView model = new ModelAndView("report");
 		
-		model.addObject("counts", ttrService.getLatestEvent().getYearCounts());
+		TrickOrTreatReportingEvent ttre = ttrService.getLatestEvent();
+		
+		if(ttre != null){
+			model.addObject("counts", ttre.getYearCounts());
+		}
 		
 		return model;
 	}
